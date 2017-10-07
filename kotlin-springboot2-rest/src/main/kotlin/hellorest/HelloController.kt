272d0f10e7c89bdf7f497d856fbf3979f4f1a0f2
Alphonse Bendt <alphonse.bendt@gmail.com>
2017-10-07 21:13:33 +0200

    use Kotlin Extensions

--- kotlin-springboot2-rest/src/main/kotlin/hellorest/HelloController.kt
+++ kotlin-springboot2-rest/src/main/kotlin/hellorest/HelloController.kt
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
 
 @RestController
 @Service
 class HelloController {
 
     @GetMapping
-    fun hello(): Mono<String> = Mono.just("Hello World!")
+    fun hello(): Mono<String> = "Hello World!".toMono()
 
 }
